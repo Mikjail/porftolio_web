77057083bcf395fde4a6f87bb89f6c016e438a23
MikjailS <mikjailsalazar@gmail.com>
2023-12-31 18:20:52 +0100

    feat: adding vite and removing unnecesary dependencies

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: "./", // the root directory of your project
+  base: "./", // the base path for your project
+  publicDir: "public", // the directory to serve as the public directory
+  build: {
+    outDir: "dist", // the output directory for the build
+  },
+};
